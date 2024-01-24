5d7ca5585926aeffd7383bf7dfbc434654e0758a
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-24 19:04:32 +0100

    add array text for cycling in

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,24 @@ export default {
   name: 'App',
   data() {
     return {
-      text: '',
-      done: true
+      slider: {
+        name: 'Todolist',
+        text: ['war machine', 'take the baby swimming', 'go to the office', 'collect the clothes', 'go shopping'],
+        done: true
+      }
     }
+  },
+  mounted() {
+    console.log(this.todo);
   }
 }
 </script>
 
-<template></template>
+<template>
+  <header>
+    <h1>{{ slider.name }}</h1>
+    <p></p>
+  </header>
+</template>
 
 <style></style>
