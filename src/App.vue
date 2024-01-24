e351a19dbd2af558a9a9cc6155b1b8462cedae1b
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-24 22:09:58 +0100

    correct and add loop for in a array of objects(list) and print in page a item for every todo

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,54 @@ export default {
   name: 'App',
   data() {
     return {
-      slider: {
-        name: 'Todolist',
-        text: ['war machine', 'take the baby swimming', 'go to the office', 'collect the clothes', 'go shopping'],
-        done: true
-      }
+      name: 'Todolist',
+      list: [
+        {
+          text: 'war machine',
+          done: true
+        },
+        {
+          text: 'take the baby swimming',
+          done: true
+        },
+        {
+          text: 'go to the office',
+          done: true
+        },
+        {
+          text: 'collect the clothes',
+          done: true
+        },
+        {
+          text: 'go shopping',
+          done: true
+        }
+      ],
     }
   },
   mounted() {
-    console.log(this.todo);
+    console.log(this.slider);
   }
 }
 </script>
 
 <template>
   <header>
-    <h1>{{ slider.name }}</h1>
-    <p></p>
+    <h1>{{ name }}</h1>
+
   </header>
+  <main>
+    <ul>
+      <li v-for="item in list">
+        {{ item.text }}
+      </li>
+    </ul>
+
+  </main>
 </template>
 
-<style></style>
+<style>
+ul {
+  color: white;
+}
+</style>
